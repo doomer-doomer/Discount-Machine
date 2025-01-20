6ddf3d22f029f94b26abb8938d2968f327113100
doomer-doomer <tejasgotavade@gmail.com>
2025-01-20 12:56:56 +0530

    added logo and meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import logo from "./logo.png";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +15,29 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Discount Machine",
   description: "Discover deals and discounts on gadgets, fashion, and more!",
+  keywords: "discounts, deals, gadgets, fashion, electronics",
+  openGraph: {
+    title: "Discount Machine",
+    description: "Discover deals and discounts on gadgets, fashion, and more!",
+    type: "website",
+    url: "https://discount-machine.vercel.app",
+    images: [{
+      url: logo.src,
+      width: 1200,
+      height: 630,
+      alt: "Discount Machine Logo",
+    }],
+    locale: 'en_US',
+    siteName: 'Discount Machine',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Discount Machine',
+    description: 'Discover deals and discounts on gadgets, fashion, and more!',
+    images: [logo.src],
+    creator: '@TeleDiscountBot',
+  },
+ 
 };
 
 export default function RootLayout({ children }) {
